29ccc0edbf37115eff6a0084cb8adf86a1256153
11032609 <104883866+11032609@users.noreply.github.com>
2022-05-11 14:44:05 +0800

    Add files via upload

---
+++ nhuOffice2010kms.bat
@@ -0,0 +1,6 @@
+c: \
+cd "C:\Program Files\Microsoft Office\Office14\"
+cscript ospp.vbs /sethst:203.72.2.180
+cscript ospp.vbs /setprt:1688
+cscript ospp.vbs /act
+pause
---
+++ nhuWin7-Win8kms.bat
@@ -0,0 +1,5 @@
+c: \
+cd c:\windows\system32
+cscript slmgr.vbs /skms 203.72.2.180:1688
+cscript slmgr.vbs /ato
+pause
